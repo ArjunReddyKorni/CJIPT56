a71554ac9cb92fc86da6d341d4cc0d445a056f45
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2023-03-11 23:44:58 +0530

    Prac tec 2-1. Some Pattern right triangle.

--- src/main/java/org/example/StringPractice.java
+++ src/main/java/org/example/StringPractice.java
@@ -127,11 +127,34 @@ public class StringPractice {
 
         for (int i = 0; i < name1.length(); i++) {
             if (name1.charAt(i) == '?' || name2.charAt(i) == '?') {
-                
+
             }
         }
     }
 
+//    public class Test
+//    {
+//        static void charNum(String inputString) {
+//            HashMap<Character, Integer> charMap = new HashMap<Character, Integer>();
+//            char[] strArray = inputString.toCharArray();
+//            for(char c: strArray) {
+//                if(charMap.containsKey(c)) {
+//                    charMap.put(c,  charMap.get(c)+1);
+//                } else {
+//                    charMap.put(c, 1);
+//                }
+//            }
+//            Set<Character> charInString = charMap.keySet();
+//            for(Character ch: charInString) {
+//                if(charMap.get(ch) > 1) {
+//                    System.out.println(ch +" : "+ charMap.get(ch));
+//                }
+//            }
+//        }
+//        public static void main(String[] args) {
+//            charNum("JavaJ2Ee");
+//        }
+//    }
 
 
 }
